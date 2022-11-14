63788268a874611821574b4ffc888340032e2c5b
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-11-14 23:34:25 +0900

    Added a module federation feature with vite-plugin-federation.

--- apps/cardpicker/src/main.tsx
+++ apps/cardpicker/src/main.tsx
@@ -1,10 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import('./bootstrap');
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export default {};
